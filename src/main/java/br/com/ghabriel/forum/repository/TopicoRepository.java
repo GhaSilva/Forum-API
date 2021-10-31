c37994682cc9f649e79b8f5d266f4fc8cd975734
Ghabriel Winicius <ghabriel-silva@outlook.com>
2021-10-31 18:09:24 -0300

    Consulta com filtros

--- src/main/java/br/com/ghabriel/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/ghabriel/forum/repository/TopicoRepository.java
@@ -1,9 +1,13 @@
 package br.com.ghabriel.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ghabriel.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
 
 }
